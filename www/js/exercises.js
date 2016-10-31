e1e1a5689f83ba1ade3bfe6845576d04bc9e14e1
Chris Aronchick <chrisaronchick@gmail.com>
2016-10-31 14:10:24 -0700

    ui-timer-exercise-list fixes

--- www/js/exercises.js
+++ www/js/exercises.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 angular.module('po10tial')
 
 .controller('exercisesCtrl', function($scope, $stateParams, $ionicListDelegate, Exercises, $ionicNavBarDelegate) {
@@ -15,75 +15,78 @@ angular.module('po10tial')
           //reject();
         }*/
       
-      exercisesVar.then(function(fromResolve) {
-        console.log("exercises returned - " + fromResolve);
+      exercisesVar.then(function (fromResolve) {
+          console.log("exercises returned - " + fromResolve);
 
-        //$scope.exercises = getExercises();
-        //console.log($scope.exercises);
-        //var allExercises = "";
-        /*while (allExercises === "") {
-          console.log(allExercises);
-          getExercises(allExercises);      
-        };*/
-        $scope.exercises = fromResolve;
-          
-        $scope.exerciseListItem = {
-          //name: '',
-          id: $scope.exercises.length
-        };
+          //$scope.exercises = getExercises();
+          //console.log($scope.exercises);
+          //var allExercises = "";
+          /*while (allExercises === "") {
+            console.log(allExercises);
+            getExercises(allExercises);      
+          };*/
+          $scope.exercises = fromResolve;
 
-        $scope.addExerciseListItem = function(exerciseListItem) {
-          if ($scope.exercises[$scope.exercises.length]) {
-            return false;
-          }
-          $scope.exercises.push({
-            name: $scope.exerciseListItem.name,
-            muscleGroup: $scope.exerciseListItem.muscleGroup,
-            id: $scope.exercises.length,
-              sets: []
-          });
-
-          $scope.exercisesListItem = {
-            name: ''
+          $scope.exerciseListItem = {
+              //name: '',
+              id: $scope.exercises.length
           };
 
-          Exercises.save($scope.exercises);
-        };
+          $scope.addExerciseListItem = function (exerciseListItem) {
+              if ($scope.exercises[$scope.exercises.length]) {
+                  return false;
+              }
+              $scope.exercises.push({
+                  name: $scope.exerciseListItem.name,
+                  muscleGroup: $scope.exerciseListItem.muscleGroup,
+                  id: $scope.exercises.length,
+                  sets: []
+              });
 
-        $scope.deleteExerciseListItem = function(index) {
-          $scope.exercises.splice(index, 1);
-          Exercises.save($scope.exercises);
-        }
-        
-        $scope.editExerciseListItem = function (exerciseName, muscleGroup) {
-            //alert(exerciseName);
-            $scope.inlineExerciseListItem = {
-                name: exerciseName,
-                muscleGroup: muscleGroup,
-                word: /^\s*\w*\s*$/
-            };
-            $ionicListDelegate.$getByHandle('exercise-list').closeOptionButtons();        
-        }
-        $scope.updateExerciseListItem = function(index, exerciseName, muscleGroup) {
-            $scope.exercises[index].name = exerciseName;
-            $scope.exercises[index].muscleGroup = muscleGroup;
-            Exercises.save($scope.exercises);
-        }
-        
-        $scope.toggleGroup = function(group) {
-          if ($scope.isGroupShown(group)) {
-            $scope.shownGroup = null;
-          } else {
-            $scope.shownGroup = group;
-          }
-        };
-        $scope.isGroupShown = function(group) {
-          return $scope.shownGroup === group;
-        };
-        
+              $scope.exercisesListItem = {
+                  name: ''
+              };
+
+              Exercises.save($scope.exercises);
+              //Exercises.all();
+          };
+
+          $scope.deleteExerciseListItem = function (index) {
+              $scope.exercises.splice(index, 1);
+              Exercises.save($scope.exercises);
+              //Exercises.all();
+          };
+
+          $scope.editExerciseListItem = function (exerciseName, muscleGroup) {
+              //alert(exerciseName);
+              $scope.inlineExerciseListItem = {
+                  name: exerciseName,
+                  muscleGroup: muscleGroup,
+                  word: /^\s*\w*\s*$/
+              };
+              $ionicListDelegate.$getByHandle('exercise-list').closeOptionButtons();
+          };
+          $scope.updateExerciseListItem = function (index, exerciseName, muscleGroup) {
+              $scope.exercises[index].name = exerciseName;
+              $scope.exercises[index].muscleGroup = muscleGroup;
+              Exercises.save($scope.exercises);
+              //Exercises.all();
+          };
+
+          $scope.toggleGroup = function (group) {
+              if ($scope.isGroupShown(group)) {
+                  $scope.shownGroup = null;
+              } else {
+                  $scope.shownGroup = group;
+              }
+          };
+          $scope.isGroupShown = function (group) {
+              return $scope.shownGroup === group;
+          };
+
       })
-      .catch(function(fromReject) {
-        console.log("No exercises returned - " + fromReject);
-      })
+      .catch(function (fromReject) {
+          console.log("No exercises returned - " + fromReject);
+      });
     
   });
--- www/js/services.js
+++ www/js/services.js
@@ -5,63 +5,63 @@ angular.module('po10tial.services', ['ionic'])
  * from local storage, and also lets us save and load the
  * last active project index.
  */
-.factory('Exercises', function($http) {
-  return {
-    all: function() {
-      var exercisesString = $http({
-        method: 'GET',
-        url: 'http://boldallies.com/cgi-bin/retrieve-exercises.php'
-      }).then(function successCallback(response) {        
-        console.log("Success");
-        if (response.data.exercisesJSON) {
-          //console.log("angular.fromJson(response.data.exercisesJSON) = ");
-          //console.log(angular.fromJson(response.data.exercisesJSON));
-          console.log("exercisesString = ");
-          console.log(angular.fromJson(response.data.exercisesJSON));
-          return angular.fromJson(response.data.exercisesJSON);
-        } else {
-          return [];
+.factory('Exercises', function ($http) {
+    return {
+        all: function () {
+            var exercisesString = $http({
+                method: 'GET',
+                url: 'http://boldallies.com/cgi-bin/retrieve-exercises.php'
+            }).then(function successCallback(response) {
+                console.log("Success");
+                if (response.data.exercisesJSON) {
+                    //console.log("angular.fromJson(response.data.exercisesJSON) = ");
+                    //console.log(angular.fromJson(response.data.exercisesJSON));
+                    console.log("exercisesString = ");
+                    console.log(angular.fromJson(response.data.exercisesJSON));
+                    return angular.fromJson(response.data.exercisesJSON);
+                } else {
+                    return [];
+                }
+            }, function errorCallback(response) {
+                console.log("errorCallback");
+                return window.localStorage['exercises'];
+            });
+            return exercisesString;
+        },
+        save: function (exercises) {
+            console.log(exercises);
+            var exercisesJSON = {
+                exercisesJSON: exercises
+            };
+            console.log(exercisesJSON);
+            $http.post("http://boldallies.com/cgi-bin/save-exercises.php", exercisesJSON)
+
+            .success(function (data, status, headers, config) {
+                console.log("Data:" + data + ", headers: " + headers);
+                //, status: " + status + ", headers: " + headers + ", config: " + config);
+            });
+            //window.localStorage['exercises'] = angular.toJson(exercises);
+        },
+        newProject: function (exerciseName) {
+            // Add a new project
+            return {
+                title: exerciseName,
+                sets: []
+            };
+        },
+        getLastActiveIndex: function () {
+            return parseInt(window.localStorage['lastActiveExercise']) || 0;
+        },
+        setLastActiveIndex: function (index) {
+            window.localStorage['lastActiveExercise'] = index;
         }
-      }, function errorCallback(response) {
-        console.log("errorCallback");
-        return window.localStorage['exercises'];
-      });
-      return exercisesString;
-    },
-    save: function(exercises) {
-      console.log(exercises);
-      var exercisesJSON = {
-        exercisesJSON: exercises
-      };
-      console.log(exercisesJSON);
-      $http.post("http://boldallies.com/cgi-bin/save-exercises.php", exercisesJSON)
-      
-      .success(function (data, status, headers, config) {
-          console.log("Data:" + data + ", headers: " + headers);
-          //, status: " + status + ", headers: " + headers + ", config: " + config);
-      });
-      //window.localStorage['exercises'] = angular.toJson(exercises);
-    },
-    newProject: function(exerciseName) {
-      // Add a new project
-      return {
-        title: exerciseName,
-        sets: []
-      };
-    },
-    getLastActiveIndex: function() {
-      return parseInt(window.localStorage['lastActiveExercise']) || 0;
-    },
-    setLastActiveIndex: function(index) {
-      window.localStorage['lastActiveExercise'] = index;
-    }
-  }
+    };
 })
 
-.factory('Exercise', function($http, exercises, set) {
+.factory('Exercise', function ($http, exercises, set) {
     return {
         save: function (set) {
             window.localStorage['exercises'] = angular.toJson(exercises);
         }
-    }
-})
+    };
+});
